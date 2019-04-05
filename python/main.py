1d173cb0811d98f3dd4750f450e5ee5ce96a8946
Juan Camilo Vargas <caraham0203@gmail.com>
2019-04-04 20:31:04 -0500

    Proyecto agregado
    Se agregan ejemplos del uso de Python

---
+++ python/main.py
@@ -0,0 +1,33 @@
+
+""" print("Hola mundo") 
+x = float(input("Ingrese dato: "))
+print(x)
+y = float(input("Ingrese dato: "))      
+print(5**3)
+a = True
+b = False
+print(a)
+print(10%2) """
+
+""" a = 5
+if a==3:
+    print("Hola")
+    print("Mundo")
+
+if a<5:
+    print("Es menor que 5")
+else:
+    print("No es menor")
+print("Curso") """
+
+""" a = 7
+while a == 7:
+    print("Estoy en el while")
+    a = int(input("Ingrese el nuevo valor de a: "))
+    if a != 7:
+        break """
+
+a = 3 
+b = 4
+if a == 3 and b == 4:
+    print("Es correcto")
